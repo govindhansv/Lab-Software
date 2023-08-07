831eb6046e99f18e3f9cc0bfd5104a2487b15c57
Govind Hans <govindhansv333@gmail.com>
2023-08-07 17:16:48 +0000

    dev added

--- routes/dev.js
+++ routes/dev.js
@@ -9,6 +9,29 @@ router.get('/', function (req, res) {
     db.get.collection('users').deleteMany({});
     res.send('Done');
 });
+router.get('/products', function (req, res) {
+
+    const dproducts = [
+        {
+            title: 'Iphone 1',
+            image: 'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-3.jpg',
+            price: 19.99,
+        },
+        {
+            title: 'Product 2',
+            image: 'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-3.jpg',
+            price: 24.99,
+        },
+        {
+            title: 'Android 2',
+            image: 'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-3.jpg',
+            price: 24.99,
+        },
+        // Add more products here
+    ];
+
+    res.json(dproducts);
+});
 
 
 module.exports = router;
